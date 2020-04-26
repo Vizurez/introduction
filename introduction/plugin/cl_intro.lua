f312eafda4552401ef0aa9c5f180949d7b7c2e21
Viz <vizurez@gmail.com>
2020-04-26 20:25:09 +0100

    Initial commit

---
+++ introduction/plugin/cl_intro.lua
@@ -0,0 +1,19 @@
+local PLUGIN = PLUGIN;
+PLUGIN.text = [[You find yourself in Serenity, a small town offering shelter just off the old US Route 95.
+The region lies around fifteen miles south of Searchlight, on the road north to Vegas - a city rumoured to be untouched by the destruction of the Great War.
+The road north to Searchlight and beyond is treacherous, going through a barren and untamed section of the Mojave Desert.
+The NCR does not patrol the northern stretch of highway and squads are rarely sent across it. Rumours of encounters with wildlife and raiders on the path north are abundant.
+However abundant the rumours are of deadly encounters are, they do not nearly match the amount of tales of those who never reached Searchlight.
+It is hard to tell just how many make the perilous journey, with no means of ever hearing from them again as they move on in the world, never to be seen again.
+
+This pocket of life in the harsh Mojave offers the chance to mould your future, to steer your path however you please.
+Like any area of America following the war, there are plenty of hidden activities and opportunities.
+Rumours of metal men walking the sands through the haze of the scorching heat are ever present and linger like the warm fresh air.
+
+Serenity offers a foothold of life and promise in a barren and desolate wasteland, with neighbouring towns and an ever present NCR garrison.
+However, the future of Serenity hangs in the balance, with the harsh conditions of the wasteland and the growing threat of Legion and raider activity in the east.
+The weak and thinly stretched NCR forces could yet be forced to leave.
+Your fate and the fate of the region will inevitably be moulded through the actions of yourself and others.
+
+This is where your story begins.
+]];
